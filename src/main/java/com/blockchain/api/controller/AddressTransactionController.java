2327cfa00619045e82bed40831cf054d53c18e8f
Sundeep <kahlon.sundeep@gmail.com>
2018-02-07 23:48:53 +0000

    Controller to get transactions.

---
+++ src/main/java/com/blockchain/api/controller/AddressTransactionController.java
@@ -0,0 +1,30 @@
+package com.blockchain.api.controller;
+
+import com.blockchain.api.domain.UnspentTransactionOutputs;
+import com.blockchain.api.services.AddressService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Component
+@RestController
+@RequestMapping("/address")
+public class AddressTransactionController {
+
+    private AddressService addressService;
+
+    public AddressTransactionController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @RequestMapping(value = "/unspent/{bitcoinAddr}", method = GET)
+    @ResponseBody
+    public UnspentTransactionOutputs getUnspentTransactionOutputs(@PathVariable String bitcoinAddr) {
+           return addressService.getUnspentTransactions(bitcoinAddr);
+    }
+
+}
