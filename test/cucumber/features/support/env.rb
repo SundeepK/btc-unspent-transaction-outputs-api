1e1b0965290670a5c48f80e3193fc2429077dec6
Sundeep <kahlon.sundeep@gmail.com>
2018-02-08 23:56:06 +0000

    UnderTest class to start up api.

--- test/cucumber/features/support/env.rb
+++ test/cucumber/features/support/env.rb
@@ -2,18 +2,22 @@ require 'rest-assured'
 require 'pry'
 require 'json_spec/cucumber'
 require 'process-helper'
+require_relative 'under_test'
 
 AfterConfiguration do |config|
-  RestAssured::Server.start(database: ':memory:', port: 9000)
-  @process = ProcessHelper::ProcessHelper.new()
-  @process.start(['../../../../run.sh'], /(Tomcat started)/)
+  RestAssured::Server.start(database: ':memory:', port: REST_ASSURED_PORT)
+  until RestAssured::Server.up?
+    puts "Waiting for RestAssured::Server to come up on port #{REST_ASSURED_PORT}..."
+    sleep 1
+  end
+  UnderTest.instance.start
 end
 
 After do |scenario|
+  RestClient.delete "#{RestAssured::Server.address}/doubles/all"
 end
 
 at_exit do
-  @process.kill
-  @process.wait_for_exit
+  UnderTest.instance.kill
 end
 
