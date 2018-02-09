ab7e0bd755f806567b285aa25bcbf987a9d5a998
Sundeep <kahlon.sundeep@gmail.com>
2018-02-09 00:06:42 +0000

    Basic error handling.

--- src/main/java/com/blockchain/api/services/AddressService.java
+++ src/main/java/com/blockchain/api/services/AddressService.java
@@ -1,31 +1,52 @@
 package com.blockchain.api.services;
 
-import com.blockchain.api.domain.UnspentTransactionOutputs;
+import com.blockchain.api.domain.response.UnspentTransactionOutputs;
+import com.blockchain.api.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class AddressService {
 
+    private static final Logger logger = LoggerFactory.getLogger(AddressService.class);
     private final RestTemplate restTemplate;
     private final String blockChainApiBaseUrl;
 
+    @Autowired
     public AddressService(final RestTemplate restTemplate,
                           @Value("${blockchain.api.base.url}") final String blockChainApiBaseUrl) {
         this.restTemplate = restTemplate;
         this.blockChainApiBaseUrl = blockChainApiBaseUrl;
     }
 
-    public UnspentTransactionOutputs getUnspentTransactions(String address) {
+    public UnspentTransactionOutputs getUnspentTransactions(String address) throws NotFoundException {
+        logger.info("url " + blockChainApiBaseUrl);
         String unspentOutputsUri = UriComponentsBuilder
                 .fromUriString(blockChainApiBaseUrl)
                 .path("unspent")
                 .queryParam("active", address)
                 .build()
                 .toUriString();
-        return restTemplate.getForObject(unspentOutputsUri, UnspentTransactionOutputs.class);
+        logger.info("Calling url " + unspentOutputsUri);
+
+        try {
+            return restTemplate.getForObject(unspentOutputsUri, UnspentTransactionOutputs.class);
+        } catch (HttpStatusCodeException exception) {
+            if (exception.getResponseBodyAsString().contains("Invalid Bitcoin Address")) {
+                throw new NotFoundException("Bitcoin address not found");
+            } else {
+                throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getResponseBodyAsString());
+            }
+        }
+
     }
 
 }
